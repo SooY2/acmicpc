3c1fc479b937dbb088cc74c307c9306b73ce511c
SooY2 <101343915+SooY2@users.noreply.github.com>
2023-01-05 18:46:26 +0900

    Create 10926.cpp

---
+++ 10926_??!/10926.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main(){
+/*
+    string str;
+
+    cin>>str;
+    cout<<str;
+    cout<<"?\?!";//다른 문자로 치환되는 삼중자가 있어 \넣어주면 됨-> 근데 c++17부터는 삼중자 없어져서 그냥 써도돼
+*/
+    char c[52];
+    cin>>c;
+    cout<<c<<"?\?!";
+    
+
+    return 0;
+}
